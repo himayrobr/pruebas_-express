1934caf2d059bd345c6ab6cacde0fcd3cb952208
himayrobr <169061000+himayrobr@users.noreply.github.com>
2024-08-29 15:56:16 -0500

    feat: :spakles: actualizaciones

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
 
-app.post('/user', (req,res) => {
-    console.log(req.body)
-    res.send('nuevo usuario creado')
+app.get('/hello/:username', (req,res) =>{
+    console.log(typeof req.params.username)
+    res.send(`hello ${req.params.username.toLocaleUpperCase()}`)
 })
 
+app.get('/add/:x/:y', (req, res) =>{
+    const{x,y} =req.params
+    res.send(`result: ${ parseInt(x) + parseInt (y)}`)
+})
+
+app.get('/users/:username/photo', (req, res) =>{
+    if (req.params.username === "sergio"){
+        return res.sendFile('./sonic.png', {
+            root: __dirname
+        })
+    }
+    res.send('el usuario no tiene acceso')
+})
+
+app.get('/nombre/:nombre/age/:age' (req,res))
 app.listen(3000);
 console.log(`server on port ${3000}`);
 
